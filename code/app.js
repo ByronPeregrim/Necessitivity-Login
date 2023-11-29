3cf1343d350493db0a55ab204c56d8d1a1c47ed1
Byron Peregrim <byronperegrim@gmail.com>
2023-11-29 14:51:17 -0500

    Added user page view. Added redirect from login to user page. Sent data to user page and successfully displayed user data on user page.

--- code/app.js
+++ code/app.js
@@ -13,6 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT;
 
+var userData = null;
+
 // Set up for account recovery emails
 const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -32,7 +34,7 @@ const connectionParams = {
 // general config
 app.set('views', __dirname + '/views'); 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -44,7 +46,7 @@ mongoose.connect(dbUrl, connectionParams).then(()=>{
 });
 
 app.listen(PORT,()=>{
-    console.log(`Listen on PORT: ${PORT}`)
+    console.log(`Listen on PORT: ${PORT}`);
 });
 
 // Save registration form data to database
@@ -52,24 +54,24 @@ app.post("/register",async (req,res)=>{
     // Check username and email for already existing username and email in user database
     const username = await UserModel.exists({
         username:req.body.username
-    })
+    });
     if (username) {
         // Username exists already, redirect to error page
-        console.log("USERNAME EXISTS ALREADY")
+        console.log("USERNAME EXISTS ALREADY");
         res.redirect('/username-exists-error');
-    }else {
+    } else {
         const email = await UserModel.exists({
             email:req.body.email
-        })
+        });
         if (email) {
             // Email exists already, redirect to error page
-            console.log("EMAIL EXISTS ALREADY")
+            console.log("EMAIL EXISTS ALREADY");
             res.redirect('/email-exists-error');
         }
         else {
             // If username and email does not already exist, add user registration data to MongoDB database
-            const userModel = UserModel(req.body)
-            userModel.save()
+            const userModel = UserModel(req.body);
+            userModel.save();
             res.redirect('/registration-successful');
         }
     }
@@ -82,7 +84,7 @@ app.post("/login-user",async (req,res)=>{
     })
     var enteredPassword = req.body.password;
     const password = await UserModel.find({_id:userID}).select('password -_id');
-    var hashedPassword = password[0].password
+    var hashedPassword = password[0].password;
     if (await bcrypt.compare(enteredPassword, hashedPassword)) {
         if (userID) {
             const admin_status = await UserModel.find({_id:userID}).select('admin -_id');
@@ -91,9 +93,11 @@ app.post("/login-user",async (req,res)=>{
                 console.log("Logging in Admin...");
             }else{
                 // REDIRECT TO USER PAGE HERE ~~~~~~~~~~~~~~
-                console.log("Logging in User...");
+                const data = await UserModel.find({_id:userID}).select('-_id');
+                userData = data[0];
+                console.log("Logging in " + userData.username + "...");
+                res.redirect('/user-page');
             }
-            res.redirect('/');
         } else {
             res.redirect('/login-error');
         }
@@ -108,7 +112,7 @@ app.post("/login-user",async (req,res)=>{
 app.post("/recover-account",async (req,res)=>{
     const email = await UserModel.exists({
         email: req.body.email
-    })
+    });
     // If email address is in database, send recovery email and redirect to success page
     if (email) {
         const userInfo = await UserModel.find({email:req.body.email}).select('username password -_id');
@@ -132,7 +136,7 @@ app.post("/recover-account",async (req,res)=>{
     else { // else redirect to page with error message
         res.redirect('/account-recovery-error');
     }
-})
+});
 
 // get functions for page redirects:
 app.get("/", (req, res)=>{
@@ -141,24 +145,30 @@ app.get("/", (req, res)=>{
 
 app.get("/login-error", (req, res)=>{
     res.render("login-error.ejs");
-})
+});
 
 app.get("/email-exists-error", (req, res)=>{
     res.render("email-exists-error.ejs");
-})
+});
 
 app.get("/username-exists-error", (req, res)=>{
     res.render("username-exists-error.ejs");
-})
+});
 
 app.get("/registration-successful", (req, res)=>{
     res.render("registration-successful.ejs");
-})
+});
 
 app.get("/account-recovery-error", (req, res)=>{
     res.render("account-recovery-error.ejs");
-})
+});
 
 app.get("/account-recovery-success", (req, res)=>{
     res.render("account-recovery-success.ejs");
-})
+});
+
+app.get("/user-page", (req, res)=>{
+    res.render("user-page.ejs", { 
+        data : userData
+    });
+});
