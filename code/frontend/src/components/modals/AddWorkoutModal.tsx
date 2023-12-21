6beeb01e27ccf03c0334439a2755819118aebd5a
Byron Peregrim <byronperegrim@gmail.com>
2023-12-20 23:48:57 -0500

    Finished backend logic for add workout modal. Added dictionary that stores key-value pairs of exercise names and MET values.

--- code/frontend/src/components/modals/AddWorkoutModal.tsx
+++ code/frontend/src/components/modals/AddWorkoutModal.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { NewWorkoutInfo } from "../../network/users_api";
 import { User } from "../../models/users";
 import moment from "moment";
+import { exercises } from "../../dictionaries/workouts";
 
 interface AddWorkoutModalProps {
     currentUser : User | null,
@@ -21,13 +22,16 @@ const AddWorkoutModal = ({currentUser, onAddWorkoutSuccessful, onBackButtonClick
         reValidateMode: "onSubmit",
     });
 
-    const [calories, setCalories] = useState(0);
+    const [duration, setDuration] = useState(0);
+    const [exercise, setExercise] = useState("");
     const [totalCalories, setTotalCalories] = useState(0);
+    const [showEstimatedCalories, setShowEstimatedCalories] = useState(0);
+    const [showEnteredCalories, setShowEnteredCalories] = useState(0);
 
     async function onSubmit(newWorkoutInfo: NewWorkoutInfo) {
         try {
             newWorkoutInfo.calories = totalCalories;
-            const newWorkout = await UsersApi.addWorkout(newWorkoutInfo);
+            await UsersApi.addWorkout(newWorkoutInfo);
             alert("Add Workout Successful!");
             onAddWorkoutSuccessful();
         } catch (error) {
@@ -41,7 +45,22 @@ const AddWorkoutModal = ({currentUser, onAddWorkoutSuccessful, onBackButtonClick
     }
 
     const updateTotalCalories = () => {
-        setTotalCalories(totalCalories + calories);
+        setTotalCalories(totalCalories + showEnteredCalories);
+    }
+
+    const calculateCalories = () => {
+        const userWeight = currentUser?.weight;
+        if (exercise !== "" && userWeight !== undefined && duration > 0) {
+            const METScore = exercises[exercise];
+            let caloriesBurned = ((METScore * 3.5 * (userWeight/2.205)) / 200) * duration;
+            caloriesBurned = Math.round(caloriesBurned);
+            setShowEstimatedCalories(caloriesBurned);
+            setShowEnteredCalories(caloriesBurned);
+        } 
+    }
+
+    const handleChange = (e: { preventDefault: () => void; target: { value: any; }; }) => {
+        setShowEnteredCalories(Number(e.target.value));
     }
 
     return ( 
@@ -50,9 +69,32 @@ const AddWorkoutModal = ({currentUser, onAddWorkoutSuccessful, onBackButtonClick
                 <div className={styles.banner_box}>
                     <h1 className={styles.banner_text}>FitTracker 5000</h1>
                 </div>
+                <div className={styles.custom_exercise_wrapper}>
+                    <label htmlFor="lang">Exercise:</label>
+                    <select name="languages" id="lang" defaultValue={""} onChange={e => setExercise(e.target.value)}>
+                        <option value="" disabled hidden>Choose exercise</option>
+                        <option value="running">Running</option>
+                        <option value="walking">Walking</option>
+                        <option value="biking">Biking</option>
+                        <option value="swimming">Swimming</option>
+                        <option value="jumping-jacks">Jumping Jacks</option>
+                        <option value="basketball">Basketball</option>
+                        <option value="weight-lifting">Weight Lifting</option>
+                    </select>
+                    <label htmlFor="duration">Duration (mins):</label>
+                    <input type="number" id="duration" name="duration" defaultValue={0} onChange={e => setDuration(Number(e.target.value))}/>
+                    <Button
+                        type="button"
+                        disabled={isSubmitting}
+                        onClick={calculateCalories}
+                    >   
+                        Enter
+                    </Button>
+                    <p>Estimated Calories: {showEstimatedCalories}</p>
+                </div>
                 <label htmlFor="calories">Enter amount of calories burned:</label>
-                <input type="number" id="calories" name="calories" placeholder="0" defaultValue={0} onChange={e => setCalories(Number(e.target.value))}/>
-                <button type="button" onClick={updateTotalCalories}>Enter</button>
+                <input type="number" id="calories" name="calories" value={showEnteredCalories.toString()} onChange={handleChange}/>
+                <button type="button" onClick={updateTotalCalories}>Add</button>
                 <Form onSubmit={handleSubmit(onSubmit)}>
                     <input type="hidden" {...register("user")} defaultValue={currentUser?._id}/>
                     <input type="hidden" {...register("calories")} defaultValue={totalCalories}/>
---
+++ code/frontend/src/dictionaries/workouts.tsx
@@ -0,0 +1,9 @@
+export var exercises: {[exercise: string] : number} = {};
+
+exercises["running"] = 11.2;
+exercises["walking"] = 3;
+exercises["biking"] = 6;
+exercises["swimming"] = 8;
+exercises["jumping-jacks"] = 6;
+exercises["basketball"] = 8;
+exercises["weight-lifting"] = 5;
